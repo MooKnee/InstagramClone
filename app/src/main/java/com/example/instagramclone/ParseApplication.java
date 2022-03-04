bba0ba0323f2537033f24b8c7a57a8c7c5f742e2
MooKnee <mooneycatie@gmail.com>
2022-03-03 23:21:54 -0500

    parse login and navigation

--- app/src/main/java/com/example/instagramclone/ParseApplication.java
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -1,4 +1,18 @@
 package com.example.instagramclone;
 
-public class ParseApplication {
+import android.app.Application;
+
+import com.parse.Parse;
+
+public class ParseApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("jMAXadq1YCxyIiHpnI18sJSwBOxx8N0nPT2Gtyqm")
+                .clientKey("uJIvni0Da2uPMAuzSImStaQ4kNJy62FYXqh4m4rC")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
 }
